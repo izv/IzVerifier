b367935f66810b4c4897cc860c5a3e2070f1890f
Ahmed Abu Lawi <aabulawi@redhat.com>
2015-05-06 13:36:00 -0400

    Update for 0.0.4.1 release

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 
 setup(
         name='IzVerifier',
-        version='0.0.4.0',
+        version='0.0.4.1',
         author='Francisco Canas',
         author_email='mailfrancisco@gmail.com',
         maintainer='Ahmed Abu Lawi',
-        maintainer_email='mail.ahmed.lawi@gmail.com',
+        maintainer_email='ahmedlawi92@gmail.com',
         packages=[
             'IzVerifier',
             'IzVerifier.exceptions',
